93b46daad125fdfafb43032aa3de73926022eaa5
Kin-ichi WATANABE <kin@soum.co.jp>
2015-11-30 22:17:50 +0900

    adds howto choreonoid.

--- build.sh
+++ build.sh
@@ -8,3 +8,9 @@ make -e SPHINXOPTS="-D language='ja'" html
 mv _build/html _html-ja
 make html
 mv _html-ja _build/html-ja
+
+make -e SPHINXOPTS="-D language='ja'" latexpdfja
+mv _build/latex _latex-ja
+#make latexpdfja
+mv _latex-ja _build/latex-ja
+
---
+++ howto/index.rst
@@ -0,0 +1,11 @@
+Choreonoidの解説
+================
+
+この章ではChoreonoidについて解説します。
+
+.. toctree::
+   :maxdepth: 2
+
+   about
+   howto-use
+   tips
